366d69d18c00d4853377a297536ac12eade7ea19
Cory Foy <github@cornetdesign.com>
2012-01-15 20:34:59 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Commonsquirrel::Application.config.secret_token = '70386838fd716fd26ff156e3ee8ef154725df2079bac1cf2ef03d81a65504f7ec2720757e5d7a1e5b13783bdd5a7a15ba4c0cdefcbfb4fe0a0dd6578e2ca79f6'
